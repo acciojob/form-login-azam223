07728c18f48556a8340e2d0f6ba216fb5c8d08c1
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 16:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Get the form elements by their name attributes
+    const firstName = document.forms["form1"]["fname"].value;
+    const lastName = document.forms["form1"]["lname"].value;
+    
+    // Display the first and last names using an alert
+    alert("First Name: " + firstName + "\nLast Name: " + lastName);
 
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
